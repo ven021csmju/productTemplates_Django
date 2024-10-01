bf46e0f374b897d2b15f22ccd1b40a6c0a0edd90
surapong <Peeraphat27277@gmail.com>
2024-10-01 23:30:05 +0700

    first commit

---
+++ productApp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+def electronics(request):
+    product_dict={
+        'product1':'Mac',
+        'product2':'IPhone',
+        'product3':'Dell'
+    }
+    return render(request, 'productApp/products.html',product_dict)
+
+def toys(request):
+    product_dict={
+        'product1':'Remoote Car',
+        'product2':'Drone',
+        'product3':'Rocket Launcher'
+    }
+    return render(request, 'productApp/products.html',product_dict)
+
+def shoes(request):
+    product_dict={
+        'product1':'Nike',
+        'product2':'Adidas',
+        'product3':'Reebok'
+    }
+    return render(request, 'productApp/products.html',product_dict)
+def index(request):
+    return render(request, 'productApp/index.html')
+
